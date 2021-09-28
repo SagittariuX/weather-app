de589ed2d2da0186e40d42ab9dfef8cb40bba25b
aaronan <aaronan@amrock.com>
2021-09-28 15:11:26 -0400

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+ 
+
+import { KelvinToCelciusPipe, KelvinToFahrenheitPipe } from 'src/pipes/weather.pipe';
+
+import { TodaysWeatherComponent } from './todays-weather/todays-weather.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
+import { ChiplistComponent } from './shared/chiplist/chiplist.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodaysWeatherComponent,
+    HeaderComponent,
+    ChiplistComponent,
+    KelvinToCelciusPipe,
+    KelvinToFahrenheitPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
